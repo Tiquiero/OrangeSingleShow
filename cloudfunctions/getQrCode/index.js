ce0d4164cc84fdce9258cb12b01625e9f4705286
曾鹏浩 <tiquiero@163.com>
2020-01-07 17:11:25 +0800

    update
    增加生成我的小程序码

--- cloudfunctions/getQrCode/index.js
+++ cloudfunctions/getQrCode/index.js
@@ -6,6 +6,12 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
-  
+  const result = await cloud.openapi.wxacode.getUnlimited({
+    scene: wxContext.OPENID,
+  })
+  const upload = await cloud.uploadFile({
+    cloudPath: 'qrcode/' + Date.now() + '-' + Math.random() + '.png',
+    fileContent: result.buffer
+  })
+  return upload.fileID
 }
